1015507df60a08161b32050949ccb49b7789fd74
bishwa <bishwa.subedi4@gmail.com>
2026-05-10 15:27:07 -0500

    added temperature conversion

--- python_practices.py
+++ python_practices.py
@@ -2,17 +2,24 @@
 # age = input("What is your age?")
 # print (f"Hello {name}, Your are {age} years old")
 
-num1= int(input("Enter any number"))
-num2 = int(input("Enter any number"))
+# num1= int(input("Enter any number"))
+# num2 = int(input("Enter any number"))
 
-add = num1 + num2
-sub = num1 - num2
-mul = num1 * num2
-div = num1 / num2
-rem = num1 % num2
+# add = num1 + num2
+# sub = num1 - num2
+# mul = num1 * num2
+# div = num1 / num2
+# rem = num1 % num2
 
-print (f"Addition = {add}\n")
-print (f"Subtraction = {sub}\n")
-print (f"Multiplication = {mul}\n")
-print (f"Division = {div}\n")
-print (f"Reminder = {rem}\n")
+# print (f"Addition = {add}\n")
+# print (f"Subtraction = {sub}\n")
+# print (f"Multiplication = {mul}\n")
+# print (f"Division = {div}\n")
+# print (f"Reminder = {rem}\n")
+
+
+C = int (input("What is the temperature today in Celsius today?"))
+
+F = (C * 9/5) +32
+
+print(f"Thats {F} Farenheit ")
