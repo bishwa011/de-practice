731a139d2ce4c6c1c172ed6c7cb11568d664a8da
bishwa <bishwa.subedi4@gmail.com>
2026-05-10 15:23:32 -0500

    commit operations

--- python_practices.py
+++ python_practices.py
@@ -1,3 +1,18 @@
-name = input("What is your name?")
-age = input("What is your age?")
-print (f"Hello {name}, Your are {age} years old")
+# name = input("What is your name?")
+# age = input("What is your age?")
+# print (f"Hello {name}, Your are {age} years old")
+
+num1= int(input("Enter any number"))
+num2 = int(input("Enter any number"))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+rem = num1 % num2
+
+print (f"Addition = {add}\n")
+print (f"Subtraction = {sub}\n")
+print (f"Multiplication = {mul}\n")
+print (f"Division = {div}\n")
+print (f"Reminder = {rem}\n")
